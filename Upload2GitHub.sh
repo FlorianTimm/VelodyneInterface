147abf06cc4b03059c4435c90714a51720880ae0
Florian Timm <flotimm@gmail.com>
2017-11-06 16:05:32 +0100

    Stand 17-11-06_16:05:32

---
+++ Upload2GitHub.sh
@@ -0,0 +1,4 @@
+git add *
+datum=$(date +%y-%m-%d_%H:%M:%S)
+git commit -m "Stand $datum"
+git push
---
+++ convTxt2Obj.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+@author: Florian Timm
+@version: 2017.10.22
+'''
+from vdPoint import VdPoint
+from vdFile import VdFile
+
+class ConvTxt2Obj:
+    def __init__ (self):
+        self.fileName = "test.txt"
+        
+    def run(self):
+        data = []
+        txt = open(self.fileName, 'rb')
+        for line in txt:
+            l = line.split()
+            data.append(VdPoint(float(l[0]),float(l[1]),float(l[2]),
+                                float(l[3]),int(l[4])))
+            #print ("test")
+        f = VdFile("obj", self.fileName)
+        f.writeDataToObj(data)
+        
+        
+if __name__ == '__main__':
+    ConvTxt2Obj().run()
+        
